2d40e24224869f4461e24b4891dfc19df3c38c91
ceit <9731127@ceit.local>
2019-01-19 11:01:21 +0330

    HI

---
+++ Untitled1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void openning(void);
+void enter_name (int max_len, char name[max_len]);
+
+
+int main()
+{
+    openning;
+}
+
+void enter_name (int max_len, char name[max_len])
+{
+    scanf("%s", name);
+    return;
+}
